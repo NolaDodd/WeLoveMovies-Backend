c965cb21af550bd404da28133bca162992e33fc4
Nola Dodd <noladodd.code@gmail.com>
2024-09-25 16:21:32 -0700

    update server.js for vercel

--- src/server.js
+++ src/server.js
@@ -3,12 +3,26 @@ const { PORT = 5001 } = process.env;
 const app = require("./app");
 const knex = require("./db/connection");
 
-const listener = () => console.log(`Listening on Port ${PORT}!`);
+const app = require("./app");
+const knex = require("./db/connection");
 
-knex.migrate
-  .latest()
-  .then((migrations) => {
-    console.log("migrations", migrations);
-    app.listen(PORT, listener);
-  })
-  .catch(console.error);
+module.exports = async (req, res) => {
+  try {
+    await knex.migrate.latest();
+
+    app(req, res);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+// const listener = () => console.log(`Listening on Port ${PORT}!`);
+
+// knex.migrate
+//   .latest()
+//   .then((migrations) => {
+//     console.log("migrations", migrations);
+//     app.listen(PORT, listener);
+//   })
+//   .catch(console.error);
